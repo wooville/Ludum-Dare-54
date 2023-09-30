6e5574d5f30b69704ce974bb886789826f011f93
Julian Woo <julian.wooville@gmail.com>
2023-09-30 11:59:39 -0400

    Interactables skeleton

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInteractable {get;set;} = false;
+    // protected Label buttonPromptLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public virtual void Interact()
+    {
+        Destroy(gameObject);
+    }
+
+    public void ToggleButtonPrompt(){
+        Debug.Log(name + " interactable");
+        // buttonPromptContainer.Visible = !buttonPromptContainer.Visible;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        // if (other.IsInGroup("Player")){
+        //     isInteractable = true;
+        //     ToggleButtonPrompt();
+        // }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other) {
+        // if (other.IsInGroup("Player")){
+        //     isInteractable = false;
+        //     ToggleButtonPrompt();
+        // }
+    }
+}
---
+++ Assets/Scripts/InventoryInteraction.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+	private List<Interactable> nearbyInteractables = new List<Interactable>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
