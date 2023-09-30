997a74fb807041712504020787a6c51b16e9f332
Carlos Murillo <crmurillo117@gmail.com>
2023-09-30 11:32:48 -0400

    Cleanup attack/shield + debug wall jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,23 +10,17 @@ public class Player : MonoBehaviour
     [SerializeField] private float _jumpSpeed;
     private Rigidbody2D _rb;
     private Sprite _sprite;
-    private Collider2D _c2d;
+    private Collider2D _bodyCollider;
+    [SerializeField] private Collider2D _footCollider;
     private float _direction;
     private bool _isFacingRight = true;
-    private bool _isAttacking = false;
-    private bool _isShielding = false;
-    public GameObject attackPoint;
-    public float attackRange;
-    [SerializeField] private int _attackDamage;
-    [SerializeField] private LayerMask enemyLayers;
-    private float _attackTimer;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _sprite = GetComponent<Sprite>();
-        _c2d = GetComponent<Collider2D>();
+        _bodyCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -39,7 +33,7 @@ public class Player : MonoBehaviour
     {
         _direction = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && _c2d.IsTouchingLayers(LayerMask.GetMask("Foreground")))
+        if (Input.GetButtonDown("Jump") && _footCollider.IsTouchingLayers(LayerMask.GetMask("Foreground")))
         {
             Vector2 jumpVelocity = new Vector2(0f, _jumpSpeed);
             _rb.velocity += jumpVelocity;
@@ -48,6 +42,7 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate() {
         _rb.velocity = new Vector2(_direction * _speed, _rb.velocity.y);
+
         FlipSprite();
         
         if (_direction != 0){
